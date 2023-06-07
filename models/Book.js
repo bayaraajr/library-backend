458840da5c62aa4599b50deb0ca6dfd4fa1886e0
aagii1104 <b200930072@erdenetis.edu.mn>
2023-06-07 11:44:27 +0800

    category

--- models/Book.js
+++ models/Book.js
@@ -2,7 +2,8 @@ const { Schema, model } = require("mongoose");
 const BookSchema = new Schema({
     isbn: String,
     name: String,
-    publicationDate: Date
+    publicationDate: Date,
+    author: String,
 });
 
 module.exports = model("book", BookSchema);
---
+++ models/CategoryList.js
@@ -0,0 +1,8 @@
+import Category from "./Category";
+
+export default function CategoryList({categories}){
+    if (!categories) 
+        return null;
+
+    return(categories);
+}
