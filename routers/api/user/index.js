4a81df84da4cf88850e06eb1a96e1cf8f0df717b
BiGoyy <120032895+BiGoyy@users.noreply.github.com>
2023-06-08 10:42:26 +0800

    feature: jwt

--- routers/api/user/index.js
+++ routers/api/user/index.js
@@ -2,12 +2,14 @@ const {
     registerUser,
     updateUser,
     login,
+    deleteUser,
 } = require("../../../controllers/user.controller");
 
 module.exports = function (fastify, opts, next) {
     fastify.post("/", registerUser);
     fastify.post("/login", login);
     fastify.put("/:email", updateUser);
+    fastify.delete("/:id", deleteUser)
     next();
 };
 
