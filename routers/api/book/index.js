8940c815bdaf1ac8125cc3a90c2770cbc845c035
aagii1104 <b200930072@erdenetis.edu.mn>
2023-06-12 15:49:44 +0800

    delete and get book function added

--- routers/api/book/index.js
+++ routers/api/book/index.js
@@ -1,13 +1,18 @@
 const {
     registerBook,
     updateBook,
+    deleteBook,
+    getBook,
 } = require("../../../controllers/book.contoller");
 const auth = require("../../../plugins/auth");
+
 // const { verifyJWT } = require("../../../utils/jwt");
 
 module.exports = function (fastify, opts, next) {
     fastify.post("/", { preHandler: auth }, registerBook);
     fastify.put("/:id", { preHandler: auth }, updateBook);
+    fastify.delete("/:id", { preHandler: auth }, deleteBook);
+    fastify.get("/find", getBook);
     next();
 };
 
