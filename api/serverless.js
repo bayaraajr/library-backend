a18e41e7c708b2df1ec29343974eb3899bec1d3e
Sumyabazar93 <sumyabazar93@gmail.com>
2023-06-15 16:26:10 +0800

    created superadmin

--- api/serverless.js
+++ api/serverless.js
@@ -12,27 +12,6 @@ server.register(multer.contentParser);
 
 server.setErrorHandler(errorHandler);
 
-exports.registerAdmin = async (req, res) => {
-    try {
-        const salt = crypto.randomBytes(20).toString("hex");
-        const hash = crypto
-            .createHash("sha256")
-            .update(req.body.password)
-            .update(crypto.createHash("sha256").update(salt, "utf8").digest("hex"))
-            .digest("hex");
-        await Admin.create({ ...req.body, hash, salt });
-
-        return res.status(201).send({
-            message: "Successfully registered a admin",
-        });
-    } catch (error) {
-        console.log(error);
-        return {
-            error: "Error",
-        };
-    }
-};
-
 server.register(require("@fastify/static"), {
     root: path.join(__dirname, "..", "public"),
     prefix: "/public/", // optional: default '/'
@@ -46,11 +25,24 @@ const startServer = async () => {
     console.log(process.env.DB_URL);
     try {
         await mongoose.connect(`${process.env.DB_URL}`);
-        /**
-         * @TODO: select admin with email
-         * if user does not exists create admin
-         * otherwise do nothing
-         * */
+
+        const salt = crypto.randomBytes(20).toString("hex");
+        const userData = {
+            firstname: "admin",
+            lastname: "super",
+            email: "superadmin@lib.mn",
+            phone: "99111111",
+            password: "12345678",
+        };
+        const hash = crypto
+            .createHash("sha256")
+            .update(userData.password)
+            .update(crypto.createHash("sha256").update(salt, "utf8").digest("hex"))
+            .digest("hex");
+
+        const admin = await Admin.findOne({ email: userData.email });
+        if (!admin) await Admin.create({ ...userData, hash, salt });
+
         server.log.info("[SERVER] Connected to MongoDB");
         server.listen({
             port: process.env.PORT,
